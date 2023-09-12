12a1da867b6d424e3ad7519c2588e19d3e08d2c7
sivakannan0008 <144804961+sivakannan0008@users.noreply.github.com>
2023-09-12 18:03:59 +0530

    Add files via upload

---
+++ challenge1.1 unit1/main.py
@@ -0,0 +1,6 @@
+def factroial(n):
+  if n==0:
+    return 1
+  else:
+    return n * factroial(n-1)
+print (factroial(10)) 
---
+++ challenge1.2 unit1/main.py
@@ -0,0 +1,5 @@
+year=400
+if(year%400==0)or(year%4==0 and year%100!=0):
+    print("Leap year")
+else:
+    print("not a leap year")
